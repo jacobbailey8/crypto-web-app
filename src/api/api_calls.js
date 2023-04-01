6c721407f92620e3a446e520041270c5e1186219
jacobbailey8 <jbaileydsi87@gmail.com>
2023-04-01 01:22:41 -0400

    installed eslint and prettier

--- src/api/api_calls.js
+++ src/api/api_calls.js
@@ -1,31 +1,31 @@
-const API_BASE = 'https://api.coingecko.com/api/v3';
+const API_BASE = 'https://api.coingecko.com/api/v3'
 
 const trending = async () => {
-    let res = await fetch(`${API_BASE}/search/trending`)
-    let data = await res.json();
-    return data.coins;
+  let res = await fetch(`${API_BASE}/search/trending`)
+  let data = await res.json()
+  return data.coins
 }
 
 const watchlistDataChange = async (list) => {
-    let formattedArray = list.map((element) => (
-        element.item.id
-    ));
-    let coinString = formattedArray.join('%2C')
-    let res = await fetch(`${API_BASE}/simple/price?ids=${coinString}&vs_currencies=usd&include_24hr_change=true`);
-    let data = await res.json();
-    return data;
+  let formattedArray = list.map((element) => element.item.id)
+  let coinString = formattedArray.join('%2C')
+  let res = await fetch(
+    `${API_BASE}/simple/price?ids=${coinString}&vs_currencies=usd&include_24hr_change=true`,
+  )
+  let data = await res.json()
+  return data
 }
 
 const getAllCoins = async () => {
-    let res = await fetch(`${API_BASE}/coins/list`);
-    let data = await res.json();
-    return data;
+  let res = await fetch(`${API_BASE}/coins/list`)
+  let data = await res.json()
+  return data
 }
 
 const singleCoinData = async (id) => {
-    let res = await fetch(`${API_BASE}/coins/${id}`);
-    let data = await res.json();
-    return data;
+  let res = await fetch(`${API_BASE}/coins/${id}`)
+  let data = await res.json()
+  return data
 }
 
-export {trending, watchlistDataChange, getAllCoins, singleCoinData};
+export { trending, watchlistDataChange, getAllCoins, singleCoinData }
--- src/components/SecondSection.jsx
+++ src/components/SecondSection.jsx
@@ -4,42 +4,34 @@ import { trending, watchlistDataChange } from '../api/api_calls';
 
 function SecondSection() {
 
-   
-    
 
     const spinLoader = () => {
         document.querySelector('#spin').classList.add('animate-spin');
         setTimeout(() => {
             document.querySelector('#spin').classList.remove('animate-spin');
         }, 1000);
-      
-    }
-    
-  return (
-    <div className='w-screen h-screen relative'>
-        <div className='m-3 flex justify-between items-center'>
-            <h1 className='text-3xl font-bold text-white'>Market Updates</h1>
-            <svg id='spin' onClick={spinLoader} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-            </svg>
-        </div>
 
-        <div className='table px-2 w-full text-white mt-6 rounded-t-lg relative'>
-            <div className='head flex text-xl rounded-t-lg p-3 bg-purple mb-4 '>
-                <div className='w-[70%] flex pl-2'>Coin</div>
-                <div className='flex justify-center w-[30%]'>Last 24h</div>
+    }
+
+    return (
+        <div className='w-screen h-screen relative'>
+            <div className='m-3 flex justify-between items-center'>
+                <h1 className='text-3xl font-bold text-white'>Market Updates</h1>
+                <svg id='spin' onClick={spinLoader} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                </svg>
             </div>
 
-           
-            
-                
-            
-       
+            <div className='table px-2 w-full text-white mt-6 rounded-t-lg relative'>
+                <div className='head flex text-xl rounded-t-lg p-3 bg-purple mb-4 '>
+                    <div className='w-[70%] flex pl-2'>Coin</div>
+                    <div className='flex justify-center w-[30%]'>Last 24h</div>
+                </div>
+            </div>
+
+
         </div>
-
-
-    </div>
-  )
+    )
 }
 
 export default SecondSection
